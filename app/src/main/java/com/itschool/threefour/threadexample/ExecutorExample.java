8a8cc59cb28daef2b6cd9cd05c19c04b2ef216e8
alesnim <alesnim@gmail.com>
2023-03-10 21:33:20 +0300

    add Work Manager Example

--- app/src/main/java/com/itschool/threefour/threadexample/ExecutorExample.java
+++ app/src/main/java/com/itschool/threefour/threadexample/ExecutorExample.java
@@ -1,10 +1,17 @@
 package com.itschool.threefour.threadexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+import androidx.work.Worker;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -13,9 +20,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorExample extends AppCompatActivity {
-    Button bt;
+    Button bt, bt_wrk;
     ImageView img;
-    float alph = 0f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +31,7 @@ public class ExecutorExample extends AppCompatActivity {
 
         bt = findViewById(R.id.bt_one);
         img = findViewById(R.id.iv_img);
-
+        bt_wrk = findViewById(R.id.bt_work);
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         bt.setOnClickListener(view -> executorService.execute(() -> {
             float alph = 0f;
@@ -46,7 +53,27 @@ public class ExecutorExample extends AppCompatActivity {
                     () -> Toast.makeText(this, "Дело сделано", Toast.LENGTH_SHORT).show());
         }));
 
+
+        bt_wrk.setOnClickListener((v) -> {
+
+            OneTimeWorkRequest work = new OneTimeWorkRequest.Builder(ExampleWorker.class).build();
+
+            WorkManager.getInstance(getApplicationContext()).enqueue(work);
+
+            WorkManager.getInstance(getApplicationContext()).getWorkInfoByIdLiveData(work.getId())
+                    .observe(this, workInfo -> {
+                        if (workInfo != null) {
+                            Log.d("TAG", "WorkInfo received: state: " + workInfo.getState());
+                            Toast.makeText(this, "Work finished", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+        });
+
+
     }
 }
+
 
 
